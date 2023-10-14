c1a35100384cf2f6e2fa1aefef4bb19c65281b6c
Joao Victor da Silva Nickler <90014083+joaonickler@users.noreply.github.com>
2023-10-14 10:55:20 -0300

    Add files via upload
    Primeiro Commit

---
+++ src/test/java/com/nkmarina/marina/NkmarinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nkmarina.marina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NkmarinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
