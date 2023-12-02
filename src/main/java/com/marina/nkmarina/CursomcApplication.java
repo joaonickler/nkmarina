9b3f36bcc10686ef78363b40adbb181a113e1938
joaonickler <joao.jvsn2@gmail.com>
2023-12-01 22:50:54 -0300

    Config CORS

--- src/main/java/com/marina/nkmarina/CursomcApplication.java
+++ src/main/java/com/marina/nkmarina/CursomcApplication.java
@@ -3,6 +3,9 @@ package com.marina.nkmarina;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -11,6 +14,20 @@ public class CursomcApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 		
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+                .allowedOriginPatterns("*") // Permitir solicitações de qualquer origem
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
+                .allowedHeaders("*") // Permitir todos os cabeçalhos
+                .allowCredentials(true); // Permitir credenciais (por exemplo, cookies)
+			}
+		};
 	}
 
 }
